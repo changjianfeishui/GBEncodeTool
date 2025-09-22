c640271b4928f554bd3e45e0c286b844a810953e
mangox <329735967@qq.com>
2025-09-22 14:37:18 +0800

    Initial commit

---
+++ Example/GBEncodeTool/MHTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MHTViewController.h
+//  GBEncodeTool
+//
+//  Created by changjianfeishui on 09/22/2025.
+//  Copyright (c) 2025 changjianfeishui. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MHTViewController : UIViewController
+
+@end
